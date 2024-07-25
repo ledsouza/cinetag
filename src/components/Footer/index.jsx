6a96a90209ef77cc29c018cc0d2f6f1983bd3b76
ledsouza <leandro.souza.159@gmail.com>
2024-07-25 15:31:21 -0300

    criando o banner

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,11 @@
+import styles from "./Footer.module.css";
+
+function Footer() {
+    return (
+        <footer className={styles.footer}>
+            <h2>Desenvolvido por Leandro Souza</h2>
+        </footer>
+    );
+}
+
+export default Footer;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,15 @@
+import Banner from "@/components/Banner";
+import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 
 function Home() {
-    return <Header />;
+    return (
+        <>
+            <Header />
+            <Banner imagem={"home"} />
+            <Footer />
+        </>
+    );
 }
 
 export default Home;
