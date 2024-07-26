1a8e5a9e973300fb19e9f3c27ff5dfac14a98850
ledsouza <leandro.souza.159@gmail.com>
2024-07-26 12:25:01 -0300

    finalizando página de favoritos

--- src/pages/Favorites/index.jsx
+++ src/pages/Favorites/index.jsx
@@ -1,14 +1,30 @@
-import Banner from "@/components/Banner";
 import styles from "./Favorites.module.css";
+
+import Banner from "@/components/Banner";
 import Title from "@/components/Title";
+import { useFavoritesContext } from "@/contexts/Favorites";
+import Card from "@/components/Card";
 
 function Favorites() {
+    const { favorites } = useFavoritesContext();
     return (
         <>
             <Banner imagem={"favoritos"} />
             <Title>
                 <h1>Meus Favoritos</h1>
             </Title>
+            <section className={styles.container}>
+                {favorites.map((favorite) => {
+                    return (
+                        <Card
+                            id={favorite.id}
+                            key={favorite.id}
+                            title={favorite.title}
+                            banner={favorite.banner}
+                        />
+                    );
+                })}
+            </section>
         </>
     );
 }
